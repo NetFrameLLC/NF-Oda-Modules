5d5431b9a0b2812ca1c443795244d8d671af1e2c
amoskivets <amoskivets>
2023-02-27 12:35:58 +0200

    add module

---
+++ shipsgo_integration/__manifest__.py
@@ -0,0 +1,21 @@
+{
+    'name': 'Shipsgo integration',
+    'version': '14.0.1.0',
+    'summary': 'Shipsgo integration',
+    'description': 'Shipsgo integration',
+    'license': 'OPL-1',
+    'depends': ['base', 'stock', 'sale'],
+    'support': 'odoo@netframe.org',
+    'website': 'https://www.netframe.org/',
+    'data': [
+       'data/shedule_action.xml',
+       'views/shipsgo_integration.xml',
+    ],
+    'images': [
+        'static/description/icon.png'
+        'static/description/banner.gif'
+    ],
+    'installable': True,
+    'auto_install': False,
+    'application': False,
+}
